d30ed69b050068e95844e974a8abbaf6f93ce05e
ShadChad <118648389+ShadChad@users.noreply.github.com>
2022-12-04 18:32:07 +0545

    Add files via upload

---
+++ 7th_day.py
@@ -0,0 +1,20 @@
+#operator
+
+a = int(input("enter the value of a?:"))
+b = int(input("enter the value of b?:"))
+
+sum = a+b #adds the value
+sub = a-b #subs the value
+div = a/b #div the value
+multi = a*b
+mod = a%b #gives the remainder
+int = a//b #gives the value on int rather than on float 
+exp = a**b #gives the square
+
+print("the sum is", sum)
+print("the sub is", sub)
+print("the div is", div)
+print("the multi is", multi)
+print("the mod is", mod)
+print("the int is", int)
+print("the exp is", exp)
