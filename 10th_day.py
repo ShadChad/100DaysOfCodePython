f57827e75655ecaf494c2d4bfec777ed915a63d5
ShadChad <118648389+ShadChad@users.noreply.github.com>
2022-12-07 23:13:42 +0545

    Add files via upload

---
+++ 10th_day.py
@@ -0,0 +1,8 @@
+a = input('enter your name')
+#--a basic progam which takes an input from the user
+print("my name is",a)
+b = input("enter a number")
+c = input("enter a second number")
+print(b+c) #will print in string
+print(int(b)+int(c)) #will print in integer
+
